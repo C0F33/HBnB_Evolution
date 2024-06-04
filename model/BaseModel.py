3c44146925df986bd158a506741448fcd1a0dbac
angel bermudez <angelo1998bg@gmail.com>
2024-06-04 18:11:58 -0400

    first batch of files

---
+++ model/BaseModel.py
@@ -0,0 +1,15 @@
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return self.__dict__.copy()
