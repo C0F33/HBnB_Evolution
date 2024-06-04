da1344fe0bb2e6dcc0b6ab92d4fc86aafacf06fd
angel bermudez <angelo1998bg@gmail.com>
2024-06-04 18:20:38 -0400

    added documentattion to BaseModel and user class

--- model/BaseModel.py
+++ model/BaseModel.py
@@ -1,12 +1,16 @@
 import uuid
 from datetime import datetime
+""" Base Model class, inherited by derived classes"""
 
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
+        """universal unique identifier created on initialization"""
         self.created_at = datetime.now()
+        """time and date of innitialization"""
         self.updated_at = datetime.now()
+        """time and date of last update"""
 
     def save(self):
         self.updated_at = datetime.now()
