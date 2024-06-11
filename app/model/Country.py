2e60b84b67b6619cc8bb0327ffa97ecd41f1a0c3
angel bermudez <angelo1998bg@gmail.com>
2024-06-10 20:26:50 -0400

    removed unused  atribute in country class

--- app/model/Country.py
+++ app/model/Country.py
@@ -4,7 +4,6 @@ class Country(BaseModel):
 	def __init__(self, name, **kwargs,):
 		super().__init__(**kwargs)
 		self.name = name
-		self.country_code = country_code
 
 	def __str__(self):
 		"""Returns a string representation of the country."""
