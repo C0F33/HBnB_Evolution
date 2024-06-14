569e4cc29631f3812f87761fd16eaf37a81841be
Thaigerhoo <jhhonathhan42@gmail.com>
2024-06-14 05:55:31 -0400

    Returned to a simpler format for BaseModel

--- model/BaseModel.py
+++ model/BaseModel.py
@@ -1,16 +1,12 @@
 import uuid
 from datetime import datetime
-""" Base Model class, inherited by derived classes"""
 
 
-class BaseModel:
-    def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        """universal unique identifier created on initialization"""
+class BaseModel():
+    def __init__(self, **kwargs):
+        self.id = uuid.uuid4()
         self.created_at = datetime.now()
-        """time and date of innitialization"""
         self.updated_at = datetime.now()
-        """time and date of last update"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
